ce6d8c3ac45efcd343ee2fd3ad39f188badfbd57
Yolanda <yeckdemies@gmail.com>
2025-03-15 12:13:20 +0100

    Contador Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,6 +1,10 @@
 import './Loader.css';
 
+let loaderCounter = 0;
+
 export const showLoader = () => {
+  loaderCounter++;
+
   let loader = document.querySelector('.loader-overlay');
   if (!loader) {
     loader = document.createElement('div');
@@ -11,8 +15,10 @@ export const showLoader = () => {
 };
 
 export const hideLoader = () => {
-  const loader = document.querySelector('.loader-overlay');
-  if (loader) {
-    loader.remove();
+  loaderCounter--;
+  if (loaderCounter <= 0) {
+    loaderCounter = 0;
+    const loader = document.querySelector('.loader-overlay');
+    if (loader) loader.remove();
   }
 };
